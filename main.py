cad9990b7f51b37156a2fde6e3c7c6bc6680bf03
talha.devx :) <talhanayyar820@gmail.com>
2026-07-23 18:18:01 +0400

    Create CRUD API for food items using FastAPI
    Implemented a CRUD API for managing food items using FastAPI. Added GET and POST endpoints to retrieve and add items to the food basket. Understood how Create and Read works with the help of HTTP methods such as GET and POST :)

--- main.py
+++ main.py
@@ -1,15 +1,48 @@
-# Creating a basic API for now 
-# Uvicorn is a small web server which is used to communicate with the browser in real-time
+# CRUD API
+"""
+Fast API ----> Is the Library we will use to create the API 
+Uvicorn ----> It is a small web server to which the browser communicates... 
+Pydantic imports strict data validation at run-time
+/ --- It is the root meaning that when you enter a building for example it is the front-desk 
+/status -- It is checking the uptime for the server or entering a specific room in the building
+"""
+
+
+"""
+Let's implement we are at the supermarket and we need to buy some items, implement CR on it... 
+"""
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
 
 app = FastAPI()
 
-@app.get("/")
+food_items = ["Milk", "Bread" , "Eggs"]
 
-def home():
-    return {
-        "name" : "John da don", 
-        "age" : 41, 
-        "message" : "I am here for a mission", 
+class Item(BaseModel):
+    name : str
+
+# GET ---> Read the items alr mentioned in the list 
+
+@app.get("/")
+def items_present():
+    return{
+        "food basket" : food_items
     }
+
+# POST ---> Create to add more items in the basket 
+
+@app.post("/items")
+def item_added(new_item: Item):
+    food_items.append(new_item.name)
+    return{
+        "status" : f"Successfully added {new_item.name}",
+        "Updated Basket" : food_items
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+
+
+
