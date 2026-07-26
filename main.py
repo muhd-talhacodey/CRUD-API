f060edf23511e81049ead98f9a9986479fa2d8eb
talha.devx :) <talhanayyar820@gmail.com>
2026-07-26 18:08:30 +0400

    Added DELETE HTTP METHOD

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+<<<<<<< HEAD
+=======
 # CRUD API
 """
 Fast API ----> Is the Library we will use to create the API 
@@ -5,8 +7,14 @@ Uvicorn ----> It is a small web server to which the browser communicates...
 Pydantic imports strict data validation at run-time
 / --- It is the root meaning that when you enter a building for example it is the front-desk 
 /status -- It is checking the uptime for the server or entering a specific room in the building
-"""
 
+if you want to carry out asynch tasks apply this 
+async def example() ----> Like this
+"""
+>>>>>>> cad9990b7f51b37156a2fde6e3c7c6bc6680bf03
+
+<<<<<<< HEAD
+=======
 
 """
 Let's implement we are at the supermarket and we need to buy some items, implement CR on it... 
@@ -41,8 +49,19 @@ def item_added(new_item: Item):
         "Updated Basket" : food_items
     }
 
+@app.delete("/delete")
+def item_remove(delete_item: str):
+    for item in food_items:
+        if item == delete_item:
+            food_items.remove(item)
+        return{
+            "New List" : food_items
+        }
+
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
 
 
 
+
+>>>>>>> cad9990b7f51b37156a2fde6e3c7c6bc6680bf03
